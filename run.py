ca15b976bf3f96b99136699b85d9da6c81e4aafb
Tristan Madden <trimad89@gmail.com>
2023-04-10 13:56:35 -0400

    pre-split

--- run.py
+++ run.py
@@ -5,6 +5,11 @@ from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 import gradio as gr
 import pandas as pd
+import numpy as np
+
+clients_sheet_range = 'Clients!A1:I1000'
+therapists_sheet_range = 'Therapists!A1:I1000'
+
 
 def authenticate_with_google_sheets():
     creds = None
@@ -29,76 +34,72 @@ def authenticate_with_google_sheets():
 def get_data(sheet_range):
     sheet_id = '1ACpGIUQ_EA42Ym_yDxNpb81DWHLXSTX1jHzq7cnNxdI'
     creds = authenticate_with_google_sheets()
-    
+
     service = build('sheets', 'v4', credentials=creds)
-    
+
     result = service.spreadsheets().values().get(spreadsheetId=sheet_id, range=sheet_range).execute()
     data = result.get('values', [])
-    
+
     if not data:
         data = []
-    
+
+    # Print the loaded data to the console
+    # print(f"Data loaded from sheet_range '{sheet_range}':")
+    # for row in data:
+    #     print(row)
+
     return data
 
-def create_vector(row_data):
-    vector = row_data[3:8]
-    vector = [float(x) for x in vector]
-    return vector
 
-def data_to_dataframe(data):
-    if data:
-        headers = data.pop(0)
-        df = pd.DataFrame(data, columns=headers)
-        df["Vector"] = df.apply(create_vector, axis=1)
-    else:
-        df = pd.DataFrame(columns=['Error'], data=[["No data found in the specified range."]])
-    return df
+def euclidean_distance(vector1, vector2):
+    return np.sqrt(np.sum((np.array(vector1) - np.array(vector2))**2))
 
-def generate_html_table(df):
-    table_html = "<table border='1'>"
-    table_html += "<tr>"
-    for col in df.columns:
-        table_html += f"<th>{col}</th>"
-    table_html += "</tr>"
+therapists_data = pd.DataFrame(get_data(therapists_sheet_range), columns=['Timestamp', 'UID', 'Name', 'Comfort_Stress', 'Comfort_Anxiety', 'Comfort_SelfConfidence', 'Comfort_Relationships', 'Comfort_Depression', 'Vector']).drop(0)
+clients_data = pd.DataFrame(get_data(clients_sheet_range), columns=['Timestamp', 'UID', 'Name', 'Stress', 'Anxiety', 'SelfConfidence', 'Relationships', 'Depression', 'Vector']).drop(0)
 
-    for i, row in df.iterrows():
-        table_html += "<tr onmouseover='this.style.backgroundColor=\"#ff3333\"' onmouseout='this.style.backgroundColor=\"\"' onclick='rowClicked({i})'>"
-        for value in row.values:
-            table_html += f"<td>{value}</td>"
-        table_html += "</tr>"
+def find_therapists(input_uid):
+    therapists_data = pd.DataFrame(get_data(therapists_sheet_range), columns=['Timestamp', 'UID', 'Name', 'Comfort_Stress', 'Comfort_Anxiety', 'Comfort_SelfConfidence', 'Comfort_Relationships', 'Comfort_Depression', 'Vector']).drop(0)
+    clients_data = pd.DataFrame(get_data(clients_sheet_range), columns=['Timestamp', 'UID', 'Name', 'Stress', 'Anxiety', 'SelfConfidence', 'Relationships', 'Depression', 'Vector']).drop(0)
 
-    table_html += "</table>"
-    return table_html
+    print(f"find_therapists input: '{input_uid}'")
+    
+    input_uid = str(int(input_uid))
 
-clients_sheet_range = 'Clients!A1:H1000'
-therapists_sheet_range = 'Therapists!A1:H1000'
+    selected_client_vector = None
+    for _, row in clients_data.iterrows():
+        if row['UID'] == input_uid:
+            vector_values = row['Vector'].split(',')
+            selected_client_vector = [float(x) for x in vector_values if x.strip()]
+            break
+    print(f"selected_client_vector: '{selected_client_vector}'")
 
-def get_data_display(sheet_range):
-    data = get_data(sheet_range)
-    df = data_to_dataframe(data)
-    table_html = generate_html_table(df)
-    custom_html = "<br><p>This is custom HTML.</p>"
-    html = table_html + custom_html
-    return html
+    closest_therapists = find_closest_therapists(selected_client_vector, therapists_data)
+    print(f"closest_therapists: '{closest_therapists}'")
 
-clients_demo = gr.Interface(
-    fn=lambda: get_data_display(clients_sheet_range),
-    inputs=[],
-    outputs="html",
-    title="TheraFit",
-    description="This interface displays Clients data from the specified Google Sheet in a table.",
-    allow_flagging="never",
-)
+    result = "Therapists sorted by their Euclidean distance (closest to farthest):\n\n"
+    for idx, therapist in enumerate(closest_therapists, start=1):
+        therapist_vector_values = therapist['Vector'].split(',')
+        therapist_vector = [float(x) for x in therapist_vector_values if x.strip()]
+        result += f"{idx}. {therapist['Name']} (UID: {therapist['UID']}, Distance: {euclidean_distance(selected_client_vector, therapist_vector):.2f})\n"
 
-therapists_demo = gr.Interface(
-    fn=lambda: get_data_display(therapists_sheet_range),
-    inputs=[],
-    outputs="html",
-    title="TheraFit",
-    description="This interface displays Therapists data from the specified Google Sheet in a table.",
-    allow_flagging="never",
-)
+    return result
 
-demo = gr.TabbedInterface([clients_demo, therapists_demo], ["Clients", "Therapists"])
+def find_closest_therapists(selected_client_vector, therapists_data):
+    if selected_client_vector is None:
+        return []
 
-demo.launch(server_port=7861)
+    therapists_distances = []
+    for _, row in therapists_data.iterrows():
+        vector_values = row['Vector'].split(',')
+        print(f"vector_values: '{vector_values}'")
+        therapist_vector = [float(x) for x in vector_values if x.strip()]
+        distance = euclidean_distance(selected_client_vector, therapist_vector)
+        therapists_distances.append((row, distance))
+
+    sorted_therapists = sorted(therapists_distances, key=lambda x: x[1])
+    #return [therapist[0] for therapist in sorted_therapists[:5]]  # Return only the top 5 closest therapists
+    return [therapist[0] for therapist in sorted_therapists]  # Return only the top 5 closest therapists
+
+iface = gr.Interface(fn=find_therapists, inputs=["text"], outputs="text", allow_flagging=True)
+
+iface.launch(share=False, server_port=7861)
