46e42197d66cad90092a6dfd32122711124789fb
Tristan Madden <trimad89@gmail.com>
2023-04-16 00:42:52 -0400

    Euclidean distance works

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 const TOKEN_PATH = path.join(process.cwd(), 'token.json');
 const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
+// Add a global variable called "data" to store client and therapist data
+let data = {
+  clients: [],
+  therapists: [],
+};
+
 /**
  * Reads previously authorized credentials from the save file.
  *
@@ -71,105 +77,139 @@ async function authorize() {
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
-app.get('/client', async (req, res) => {
+async function fetchData() {
+
+  const id = "1ACpGIUQ_EA42Ym_yDxNpb81DWHLXSTX1jHzq7cnNxdI"
+  const auth = await authorize();
+  const sheets = google.sheets({ version: 'v4', auth });
+
+  // Get the sheet properties to find the last row with data for Clients
+  const clientSheetPropertiesResponse = await sheets.spreadsheets.get({
+    spreadsheetId: id,
+    ranges: ['Clients!A1:I'],
+    fields: 'sheets(properties(sheetId,title,gridProperties(rowCount)))',
+  });
+
+  const clientSheetProperties = clientSheetPropertiesResponse.data.sheets[0].properties;
+  const clientLastRow = clientSheetProperties.gridProperties.rowCount;
+
+  // Fetch client data
+  const clientResponse = await sheets.spreadsheets.values.get({
+    spreadsheetId: id,
+    range: `Clients!A1:I${clientLastRow}`,
+  });
+  data.clients = clientResponse.data.values || [];
+
+  // Get the sheet properties to find the last row with data for Therapists
+  const therapistSheetPropertiesResponse = await sheets.spreadsheets.get({
+    spreadsheetId: id,
+    ranges: ['Therapists!A1:I'],
+    fields: 'sheets(properties(sheetId,title,gridProperties(rowCount)))',
+  });
+
+  const therapistSheetProperties = therapistSheetPropertiesResponse.data.sheets[0].properties;
+  const therapistLastRow = therapistSheetProperties.gridProperties.rowCount;
+
+  // Fetch therapist data
+  const therapistResponse = await sheets.spreadsheets.values.get({
+    spreadsheetId: id,
+    range: `Therapists!A1:I${therapistLastRow}`,
+  });
+  data.therapists = therapistResponse.data.values || [];
+}
+
+
+// Fetch data when server starts and start the server
+fetchData()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to fetch data:', error);
+  });
+
+// DATA
+app.get('/data', async (req, res) => {
+  console.log('\x1b[36m%s\x1b[0m', req.url, new Date().toLocaleString());
+  try {
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while fetching data');
+  }
+});
+
+app.get('/data/clients', async (req, res) => {
   console.log('\x1b[36m%s\x1b[0m', req.url, new Date().toLocaleString());
 
   try {
-    const auth = await authorize();
     const searchQuery = req.query.search || ''; // Get the search query from request
     console.log(searchQuery);
 
-    const sheets = google.sheets({ version: 'v4', auth });
-
-    // Get the sheet properties to find the last row with data
-    const sheetPropertiesResponse = await sheets.spreadsheets.get({
-      spreadsheetId: '1ACpGIUQ_EA42Ym_yDxNpb81DWHLXSTX1jHzq7cnNxdI',
-      ranges: ['Clients!A1:I'],
-      fields: 'sheets(properties(sheetId,title,gridProperties(rowCount)))',
-    });
-
-    const sheetProperties = sheetPropertiesResponse.data.sheets[0].properties;
-    const lastRow = sheetProperties.gridProperties.rowCount;
-
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: '1ACpGIUQ_EA42Ym_yDxNpb81DWHLXSTX1jHzq7cnNxdI',
-      range: `Clients!A1:I${lastRow}`, // Use the last row with data
-    });
-
-    const rows = response.data.values;
-    if (!rows || rows.length === 0) {
-      res.json([]);
+    let filteredClients = [];
+    if (!data.clients || data.clients.length === 0) {
+      filteredClients = [];
     } else {
       // Filter the rows based on the search query in columns 1 and 2
-      const filteredRows = rows.filter((row, index) => {
+      filteredClients = data.clients.filter((row, index) => {
         // Always include the first row (index 0)
         if (index === 0) return true;
 
         // Check if row[1] or row[2] contains the searchQuery (case-insensitive)
         return row[1].toLowerCase().includes(searchQuery.toLowerCase()) || row[2].toLowerCase().includes(searchQuery.toLowerCase());
       });
-
-      res.json(filteredRows);
     }
+
+    res.json({
+      clients: filteredClients,
+      therapists: data.therapists,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send('An error occurred while fetching data');
   }
 });
 
-app.get('/therapist', async (req, res) => {
+app.get('/data/therapists', async (req, res) => {
   console.log('\x1b[36m%s\x1b[0m', req.url, new Date().toLocaleString());
 
   try {
-    const auth = await authorize();
     const searchQuery = req.query.search || ''; // Get the search query from request
+    console.log(searchQuery);
 
-    const sheets = google.sheets({ version: 'v4', auth });
-
-    // Get the sheet properties to find the last row with data
-    const sheetPropertiesResponse = await sheets.spreadsheets.get({
-      spreadsheetId: '1ACpGIUQ_EA42Ym_yDxNpb81DWHLXSTX1jHzq7cnNxdI',
-      ranges: ['Therapists!A1:I'],
-      fields: 'sheets(properties(sheetId,title,gridProperties(rowCount)))',
-    });
-
-    const sheetProperties = sheetPropertiesResponse.data.sheets[0].properties;
-    const lastRow = sheetProperties.gridProperties.rowCount;
-
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: '1ACpGIUQ_EA42Ym_yDxNpb81DWHLXSTX1jHzq7cnNxdI',
-      range: `Therapists!A1:I${lastRow}`, // Use the last row with data
-    });
-
-    const rows = response.data.values;
-    if (!rows || rows.length === 0) {
-      console.log('No data found.');
-      res.json([]);
+    let filteredTherapists = [];
+    if (!data.therapists || data.therapists.length === 0) {
+      filteredTherapists = [];
     } else {
       // Filter the rows based on the search query in columns 1 and 2
-      const filteredRows = rows.filter((row, index) => {
+      filteredTherapists = data.therapists.filter((row, index) => {
         // Always include the first row (index 0)
         if (index === 0) return true;
 
         // Check if row[1] or row[2] contains the searchQuery (case-insensitive)
         return row[1].toLowerCase().includes(searchQuery.toLowerCase()) || row[2].toLowerCase().includes(searchQuery.toLowerCase());
       });
-
-      res.json(filteredRows);
     }
+
+    res.json({
+      clients: data.clients,
+      therapists: filteredTherapists,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send('An error occurred while fetching data');
   }
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-
-const globalVariable = "This is a global variable";
-app.get('/', (req, res) => {
-  res.render('index', { globalVariable });
+app.get('/data/update', async (req, res) => {
+  console.log('\x1b[36m%s\x1b[0m', req.url, new Date().toLocaleString());
+  try {
+    fetchData()
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while fetching data');
+  }
 });
